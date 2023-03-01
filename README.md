80a5b46eac373bfa81af9d00555deb36bb5547ce
n-mat <ngit@mailbox.org>
2023-03-01 19:18:38 +0100

    added adore_scheduling make files

---
+++ README.md
@@ -0,0 +1,19 @@
+<!--
+********************************************************************************
+* Copyright (C) 2017-2023 German Aerospace Center (DLR). 
+* Eclipse ADORe, Automated Driving Open Research https://eclipse.org/adore
+*
+* This program and the accompanying materials are made available under the 
+* terms of the Eclipse Public License 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0 
+*
+* Contributors: 
+*   Matthias Nichting - initial API and implementation
+********************************************************************************
+-->
+
+# adore_scheduling
+This repository provides scheduling functionalities for [ADORe](https://github.com/eclipse/adore) and its ROS interface. It contains the subprojects lib_adore_scheduling, adore_if_ros_scheduling_msg, adore_if_ros_scheduling and adore_scheduler.
+
---
+++ adore_scheduling.mk
@@ -0,0 +1,43 @@
+# This Makefile contains useful targets that can be included in downstream projects.
+
+ifndef adore_scheduling_MAKEFILE_PATH
+
+MAKEFLAGS += --no-print-directory
+
+.EXPORT_ALL_VARIABLES:
+adore_scheduling_project=adore_scheduling
+ADORE_SCHEDULING_PROJECT=${adore_scheduling_project}
+
+adore_scheduling_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
+make_gadgets_PATH:=${adore_scheduling_MAKEFILE_PATH}/make_gadgets
+REPO_DIRECTORY:=${adore_scheduling_MAKEFILE_PATH}
+
+adore_scheduling_tag:=$(shell cd ${make_gadgets_PATH} && make get_sanitized_branch_name REPO_DIRECTORY=${REPO_DIRECTORY})
+ADORE_SCHEDULING_TAG=${adore_scheduling_tag}
+
+adore_scheduling_image=${adore_scheduling_project}:${adore_scheduling_tag}
+ADORE_SCHEDULING_IMAGE=${adore_scheduling_image}
+
+include adore_scheduler/adore_scheduler.mk
+
+
+.PHONY: build_adore_scheduling 
+build_adore_scheduling: build_lib_adore_scheduling build_adore_if_ros_scheduling_msg build_adore_if_ros_scheduling build_adore_scheduler ## Build adore_scheduling
+	
+
+.PHONY: clean_adore_scheduling
+clean_adore_scheduling: clean_lib_adore_scheduling clean_adore_if_ros_scheduling_msg clean_adore_if_ros_scheduling clean_adore_scheduler ## Clean adore_scheduling build artifacts
+	
+
+.PHONY: branch_adore_scheduling
+branch_adore_scheduling: branch_lib_adore_scheduling branch_adore_if_ros_scheduling_msg branch_adore_if_ros_scheduling branch_adore_scheduler ## Returns the current docker safe/sanitized branch for the projects within adore_scheduling
+	
+.PHONY: image_adore_scheduling
+image_adore_scheduling: image_lib_adore_scheduling image_adore_if_ros_scheduling_msg image_adore_if_ros_scheduling image_adore_scheduler ## Returns the current docker image names for the projects within adore_scheduling
+	
+
+.PHONY: update_adore_scheduling
+update_adore_scheduling:
+	cd "${adore_scheduling_MAKEFILE_PATH}" && git pull
+
+endif
