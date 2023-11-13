e8ba48f0c218fd280197017fa796a9c0b7f91320
n-mat <ngit@mailbox.org>
2023-11-13 15:46:15 +0100

    added requirements.cmake file for adore_scheduler

---
+++ adore_basenode/requirements.cmake
@@ -0,0 +1,4 @@
+find_package(ament_cmake REQUIRED)
+find_package(rclcpp REQUIRED)
+find_package(adore_ros2_msgs REQUIRED)
+find_package(ament_lint_auto REQUIRED)
---
+++ adore_scheduler/requirements.cmake
@@ -0,0 +1,9 @@
+find_package(ament_cmake REQUIRED)
+find_package(rclcpp REQUIRED)
+find_package(rosgraph_msgs REQUIRED)
+find_package(adore_ros2_msgs REQUIRED)
+find_package(std_msgs REQUIRED)
+find_package(builtin_interfaces REQUIRED)
+find_package(adore_basenode REQUIRED)
+find_package(ament_lint_auto REQUIRED)
+
